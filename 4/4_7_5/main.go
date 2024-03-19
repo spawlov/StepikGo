2348274d2abd6736a7e16197f2e3969649d0c952
sergey <spawlov@gmail.com>
2024-03-19 19:58:26 +0700

    added new task

--- 4/4_7_5/main.go
+++ 4/4_7_5/main.go
@@ -1,1 +1,26 @@
 package main
+
+import . "fmt"
+
+func main() {
+	var n int
+	_, _ = Scan(&n)
+	factors := []int{}
+    for n%2 == 0 {
+		factors = append(factors, 2)
+        n = n / 2
+	}
+	for i := 3; i*i <= n; i = i + 2 {
+		for n%i == 0 {
+            factors = append(factors, i)
+            n = n / i
+        }
+    }
+	if n > 2 {
+        factors = append(factors, n)
+    }
+	for _, factor := range factors {
+		Print(factor, " ")
+	}
+	Println()
+}
