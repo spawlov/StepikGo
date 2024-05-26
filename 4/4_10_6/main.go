ed5e1c0e090ed6b99ef3c9f484e5119179a39002
sergey <spawlov@gmail.com>
2024-05-26 12:57:42 +0700

    Added task 4_10_6

--- 4/4_10_6/main.go
+++ 4/4_10_6/main.go
@@ -1,2 +1,26 @@
 package main
 
+import (
+	. "fmt"
+)
+
+func main() {
+	var N int
+	_, _ = Scan(&N)
+
+	maxSpeed := 0
+	fixSpeed := "NO"
+
+	for i := 0; i < N; i++ {
+		var speed int
+
+		_, _ = Scan(&speed)
+		if speed > maxSpeed {
+			maxSpeed = speed
+		}
+		if speed < 30 {
+			fixSpeed = "YES"
+		}
+	}
+	Print(maxSpeed, " ", fixSpeed, "\n")
+}
