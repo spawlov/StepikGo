807ae8b335272fd0cfe9a6071ff774e403d8cb3d
sergey <spawlov@gmail.com>
2024-05-31 00:32:15 +0700

    Added task 5_1_11

--- 5/5_1_11/main.go
+++ 5/5_1_11/main.go
@@ -1,12 +1,28 @@
 package main
 
+import "fmt"
+
 func swapNeighborElements(arr []int) {
 	length := len(arr)
 	for i := 0; i < length-1; i += 2 {
-			arr[i], arr[i+1] = arr[i+1], arr[i]
+		arr[i], arr[i+1] = arr[i+1], arr[i]
 	}
 }
 
-func main(){
+func main() {
+	var N int
 
+	_, _ = fmt.Scan(&N)
+
+	numbers := make([]int, N)
+	for i := 0; i < N; i++ {
+		_, _ = fmt.Scan(&numbers[i])
+	}
+
+	swapNeighborElements(numbers)
+
+	for _, value := range numbers {
+		fmt.Print(value, " ")
+	}
+	fmt.Print("\n")
 }
